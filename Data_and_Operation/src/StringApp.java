9c117203bcb0b9a74e16624e5f010869626520b3
jopulation <jopulation@yonsei.ac.kr>
2021-07-27 22:12:16 +0900

    JAVA1 - 6.5. 문자열 다루기

---
+++ Data_and_Operation/src/StringApp.java
@@ -0,0 +1,19 @@
+public class StringApp {
+    public static void main(String[] args) {
+        
+        // Charater vs String
+        System.out.println("Hello World"); // string
+        System.out.println('H'); // character
+        System.out.println("H"); // string
+
+        System.out.println("Hello " 
+                + "World");
+
+        // new line
+        System.out.println("Hello \nWorld");
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+
+    }
+}
